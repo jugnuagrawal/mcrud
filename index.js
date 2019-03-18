2f14ef3204344a57879372b0410280e74e94065f
Jugnu Agrawal <jugnu@firefly.local>
2019-03-19 00:56:42 +0530

    fixes after test run

--- index.js
+++ index.js
@@ -1,134 +1,133 @@
+const log4js = require('log4js');
 const MongoClient = require('mongodb').MongoClient;
 
 const utils = require('./utils');
 
+const logger = log4js.getLogger('mcrud');
+
+logger.level = process.env.LOG_LEVEL || 'info';
+
 module.exports.getCRUDMethods = (options = { url: 'mongodb://localhost:27017', database: 'mcrud', collection: '' }) => {
-    let collection;
-    let error;
-    MongoClient.connect(options.url, (err, client) => {
-        error = err;
-        collection = client.db(options.database).collection(options.collection);
-        collection.find();
-    });
     const e = {};
     e.count = (filter) => {
         return new Promise((resolve, reject) => {
-            if (error) {
-                reject(error);
-                return;
-            }
             try {
                 if (!filter) {
                     filter = {}
                 }
-                collection.find(filter).count((err, doc) => {
-                    if (err) {
-                        reject(err);
-                        return;
-                    }
-                    resolve(doc);
+                MongoClient.connect(options.url, (err1, client) => {
+                    if (err1) throw err1;
+                    const collection = client.db(options.database).collection(options.collection);
+                    collection.find(filter).count((err2, doc) => {
+                        if (err2) throw err2;
+                        resolve(doc);
+                        client.close();
+                    });
                 });
             } catch (e) {
-                reject(err);
+                logger.error(e);
+                reject(e);
             }
         });
     };
-    e.get = (options = { filter: {}, sort: '', page: 1, count: 30 }) => {
+    e.get = (params = { filter: {}, sort: '', page: 1, count: 30 }) => {
         return new Promise((resolve, reject) => {
-            if (error) {
-                reject(error);
-                return;
-            }
             try {
-                if (!options) {
-                    options = {};
+                if (!params) {
+                    params = {};
                 }
-                if (!options.filter) {
-                    options.filter = {};
+                if (!params.filter) {
+                    params.filter = {};
                 }
-                if (!options.count) {
-                    options.count = 30;
+                if (!params.count) {
+                    params.count = 30;
                 }
-                if (!options.page) {
-                    options.page = 1;
+                if (!params.page) {
+                    params.page = 1;
                 }
-                let query = collection.find(options.filter);
-                if (!options.count == -1) {
-                    const skip = (options.page - 1) * options.count;
-                    query = query.limit(options.count).skip(skip);
-                }
-                if (!options.sort) {
-                    query = query.sort(options.sort);
-                }
-                query.toArray((err, doc) => {
-                    if (err) {
-                        reject(err);
-                        return;
+                MongoClient.connect(options.url, (err1, client) => {
+                    if (err1) throw err1;
+                    const collection = client.db(options.database).collection(options.collection);
+                    let query = collection.find(params.filter);
+                    if (params.count != -1) {
+                        const skip = (params.page - 1) * params.count;
+                        query = query.limit(params.count).skip(skip);
                     }
-                    resolve(doc);
+                    if (params.sort) {
+                        query = query.sort(getSortObject(params.sort));
+                    }
+                    query.toArray((err2, doc) => {
+                        if (err2) throw err2;
+                        resolve(doc);
+                        client.close();
+                    });
                 });
             } catch (e) {
+                logger.error(e);
                 reject(e);
             }
         });
     };
     e.post = (data) => {
         return new Promise((resolve, reject) => {
-            if (error) {
-                reject(error);
-                return;
-            }
             try {
-                generateIdIfNot(data).then(newData => {
-                    collection.insert(newData, (err, doc) => {
-                        if (err) {
-                            reject(err);
-                            return;
-                        }
-                        resolve(doc)
+                MongoClient.connect(options.url, (err1, client) => {
+                    if (err1) throw err1;
+                    const collection = client.db(options.database).collection(options.collection);
+                    generateIdIfNot(data).then(newData => {
+                        collection.insert(newData, (err2, doc) => {
+                            if (err2) throw err2;
+                            resolve(doc)
+                            client.close();
+                        });
+                    }).catch(err => {
+                        reject(err);
                     });
-                }).catch(err => {
-                    reject(err);
                 });
             } catch (e) {
+                logger.error(e);
                 reject(e);
             }
         });
     };
-    e.put = (id, data) => {
+    e.put = (filter, data) => {
         return new Promise((resolve, reject) => {
-            if (error) {
-                reject(error);
-                return;
-            }
             try {
-                collection.findOneAndUpdate({ _id: id }, data, (err, doc) => {
-                    if (err) {
-                        reject(err)
-                        return;
-                    }
-                    resolve(doc)
+                if (!filter) {
+                    filter = {};
+                }
+                MongoClient.connect(options.url, (err1, client) => {
+                    if (err1) throw err1;
+                    const collection = client.db(options.database).collection(options.collection);
+                    collection.update(filter, data, (err2, doc) => {
+                        if (err2) throw err2;
+                        resolve(doc)
+                        client.close();
+                    });
                 });
             } catch (e) {
+                logger.error(e);
                 reject(e);
             }
         });
     };
-    e.delete = (id) => {
+    e.delete = (filter) => {
         return new Promise((resolve, reject) => {
-            if (error) {
-                reject(error);
-                return;
-            }
             try {
-                collection.findOneAndDelete({ _id: id }, (err, doc) => {
-                    if (err) {
-                        reject(err)
-                        return;
-                    }
-                    resolve(doc)
+                if (!filter) {
+                    filter = {};
+                }
+                MongoClient.connect(options.url, (err1, client) => {
+                    if (err1) throw err1;
+                    const collection = client.db(options.database).collection(options.collection);
+                    collection.deleteMany(filter, (err2, doc) => {
+                        if (err2) throw err2
+                        resolve(doc)
+                        client.close();
+                    });
                 });
             } catch (e) {
+                logger.error(e);
                 reject(e);
             }
         });
@@ -146,6 +145,28 @@ module.exports.getCRUDMethods = (options = { url: 'mongodb://localhost:27017', d
                 });
             }
         });
+    }
+    function getSortObject(sort) {
+        try {
+            let temp = sort.split(',');
+            temp = temp.map(e => {
+                let key = e;
+                if (key.startsWith('-')) {
+                    key = key.substr(1, key.length);
+                }
+                return Object.defineProperty({}, key, {
+                    value: e.startsWith('-') ? -1 : 1,
+                    enumerable: true,
+                    writable: true,
+                    configurable: true
+                });
+            });
+            temp = Object.assign.apply({}, temp);
+            return temp;
+        } catch (e) {
+            logger.error(e);
+            throw e;
+        }
     }
     return e;
 };
--- utils.js
+++ utils.js
@@ -1,4 +1,9 @@
+const log4js = require('log4js');
 const MongoClient = require('mongodb').MongoClient;
+
+const logger = log4js.getLogger('mcrud');
+
+logger.level = process.env.LOG_LEVEL || 'info';
 
 function generateId(name, length, next) {
     let prefix = name.split('').slice(0, 3).join('').toUpperCase();
@@ -16,38 +21,31 @@ function generateId(name, length, next) {
 
 module.exports.getNextId = (options) => {
     return new Promise((resolve, reject) => {
-        MongoClient.connect(options.url, (err1, client) => {
-            if (err1) {
-                reject(err1);
-                return;
-            }
-            const collection = client.db(options.database).collection('counter');
-            collection.findOne({ _id: options.collection }, (err2, doc) => {
-                if (err2) {
-                    reject(err1);
-                    return;
-                }
-                if (!doc) {
-                    collection.insert({ _id: options.collection, next: 2 }, (err3, doc2) => {
-                        if (err3) {
-                            reject(err3);
-                            return;
-                        }
-                        const id = generateId(options.collection, 8, 1);
-                        resolve(id);
-                    });
-                } else {
-                    const id = generateId(options.collection, 8, doc.next);
-                    const nextVal = parseInt(doc.next + '') + 1;
-                    collection.findOneAndUpdate({ _id: options.collection }, { $set: { next: nextVal } }, (err3, doc2) => {
-                        if (err3) {
-                            reject(err3);
-                            return;
-                        }
-                        resolve(id);
-                    });
-                }
+        try {
+            MongoClient.connect(options.url, (err1, client) => {
+                if (err1) throw err1;
+                const collection = client.db(options.database).collection('counter');
+                collection.findOne({ _id: options.collection }, (err2, doc) => {
+                    if (err2) throw err2;
+                    if (!doc) {
+                        collection.insert({ _id: options.collection, next: 2 }, (err3, doc2) => {
+                            if (err3) throw err3;
+                            const id = generateId(options.collection, 8, 1);
+                            resolve(id);
+                        });
+                    } else {
+                        const id = generateId(options.collection, 8, doc.next);
+                        const nextVal = parseInt(doc.next + '') + 1;
+                        collection.findOneAndUpdate({ _id: options.collection }, { $set: { next: nextVal } }, (err3, doc2) => {
+                            if (err3) throw err3;
+                            resolve(id);
+                        });
+                    }
+                });
             });
-        });
+        } catch (e) {
+            logger.error(e);
+            reject(e);
+        }
     });
 };
